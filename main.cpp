e69a762841e1397f5a59e3928a830ce33e1db750
Ayushmaan Verma <ayushmaankumarverma@gmail.com>
2026-05-28 10:07:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,5 @@
 using namespace linalg;
 using namespace std;
 int main() {
-    matrix m (3,3);
-    m.input();
-    // m.print();
-    // matrix q = m.qr_decomp_q();
-    // matrix r = m.qr_decomp_r();
-    // q.print();
-    // cout << endl;
-    // r.print();
-    matrix eigen = m.eigenvalues();
-    cout << endl;
-    eigen.print();
+    //Enter your code here.
 }
